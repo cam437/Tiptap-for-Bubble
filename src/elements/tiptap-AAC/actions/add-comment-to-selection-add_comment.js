7436e7d77235051294f48a2760393d3c75dfba9b
Cam Blunt <cam@flourishingworld.com>
2026-05-14 15:02:21 +0100

    Native formatting toolbar with Flourish theme + remove duplicate add_comment action

--- src/elements/tiptap-AAC/actions/add-comment-to-selection-add_comment.js
+++
@@ -1,8 +0,0 @@
-if (!instance.data.editor_is_ready)
-    return instance.data.returnAndReportErrorIfEditorNotReady("Add comment to selection");
-
-  if (instance.data.ext.comment) {
-    instance.data.editor.chain().focus().setComment(properties.comment_id).run();
-  } else {
-    console.log("tried to Add comment, but Comment extension is not active.");
-  }
